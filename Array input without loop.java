bcc1751cc4744778e4c0de4327b152ee8c5cb5f1
Aditi Neharkar <neharkaraditi77@gmail.com>
2025-06-16 18:56:18 +0530

    Create Array input without loop.java

---
+++ Array input without loop.java
@@ -0,0 +1,16 @@
+package Aditi;
+//Array input without loop
+import java.util.*;
+public class P36 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int[] arr=new int[5];
+		System.out.println("Enter array elements:");
+		arr[0]=sc.nextInt();
+		arr[1]=sc.nextInt();
+		arr[2]=sc.nextInt();
+		arr[3]=sc.nextInt();
+		arr[4]=sc.nextInt();
+		System.out.println(Arrays.toString(arr));
+	}
+}
