86565a5be1c2720653f80fa04397291233458bdb
Aditi Neharkar <neharkaraditi77@gmail.com>
2025-06-16 19:08:54 +0530

    Create Array updation.java

---
+++ Array updation.java
@@ -0,0 +1,16 @@
+package Aditi;
+//Array updation 
+public class P37 {
+	public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        // Before update
+        System.out.println("Before update: " + arr[2]);
+
+        // Updating value at index 2
+        arr[2] = 99;
+
+        // After update
+        System.out.println("After update: " + arr[2]);
+    }
+}
