750cd38ff0204b87d691d7611f3e7d11e2664307
Aditi Neharkar <neharkaraditi77@gmail.com>
2025-06-16 13:56:19 +0530

    Create array creation.java

---
+++ array creation.java
@@ -0,0 +1,20 @@
+package Aditi;
+import java.util.*;
+//Array Creation
+public class P34 {
+    public static void main(String[] args) {
+        // Create an integer array of size 5
+        int[] numbers = new int[5];
+
+        // Assigning values to the array
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        // Printing the values of the array
+        System.out.println(Arrays.toString(numbers));
+        
+        }
+    }
