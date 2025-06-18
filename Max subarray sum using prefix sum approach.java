a952ad21b3b69c1f82a9395e6730cb42fb1d23f3
Aditi Neharkar <neharkaraditi77@gmail.com>
2025-06-18 18:28:08 +0530

    Update Max subarray sum using prefix sum approach.java

--- Max subarray sum using prefix sum approach.java
+++ Max subarray sum using prefix sum approach.java
@@ -1,33 +1,33 @@
 package Aditi;
-// Max subarray sum using prefix sum approach
-public class P46 {
+//Max Subarray Sum using Prefix Sum
+public class P47 {
 	public static void maxSubarraySum(int numbers[]) {
 		int currSum = 0;
 		int maxSum = Integer.MIN_VALUE;
+
+		// Step 1: Create and fill prefix array
 		int prefix[] = new int[numbers.length];
 		prefix[0] = numbers[0];
 
-		// calculate prefix array
 		for (int i = 1; i < prefix.length; i++) {
 			prefix[i] = prefix[i - 1] + numbers[i];
 		}
 
-		// check all subarrays
-		for (int i = 0; i < numbers.length; i++) {
-			int start = i;
-			for (int j = i; j < numbers.length; j++) {
-				int end = j;
+		// Step 2: Calculate all subarray sums using prefix
+		for (int start = 0; start < numbers.length; start++) {
+			for (int end = start; end < numbers.length; end++) {
 				currSum = (start == 0) ? prefix[end] : prefix[end] - prefix[start - 1];
-				if (maxSum < currSum) {
+				if (currSum > maxSum) {
 					maxSum = currSum;
 				}
 			}
 		}
+
 		System.out.println("Max subarray sum = " + maxSum);
 	}
 
 	public static void main(String args[]) {
-		int numbers[] = { 1, -2, 3, -45 };
+		int numbers[] = {1, 2, 3, 45};
 		maxSubarraySum(numbers);
 	}
 }
